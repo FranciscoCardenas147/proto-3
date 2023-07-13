0546e2f68764dff6d45a89dbad7e72a8e23e8bda
FranciscoCardenas147 <135192587+FranciscoCardenas147@users.noreply.github.com>
2023-07-13 19:55:06 -0400

    se agrega Cantidad

--- script.js
+++ script.js
@@ -8,6 +8,12 @@ function calcular(){
     let netoAl25 = inputNeto * 2.5;
     let netoAl25menosPorcentaje = netoAl25 -( netoAl25 * (inputPorcentaje / 100));
     document.getElementById("p1").innerHTML = `<b>Precio Venta menos el ${inputPorcentaje}% = ${netoAl25menosPorcentaje}</b>`;
+  
+    let cantidad = parseInt(document.getElementById("cantidad1").value);
+
+    let masCantidad = netoAl25menosPorcentaje * cantidad;
+
+    document.getElementById("c1").innerHTML = `El precio * ${cantidad} es igual a ${masCantidad}`
   }
   
   
@@ -18,7 +24,13 @@ function calcular(){
     let inputPorcentaje2 = parseFloat(document.getElementById("descuento2").value);
     let brutoAl21MenosDescuento = brutoAl21 - (brutoAl21 * (inputPorcentaje2 /100)); 
   document.getElementById("p2").innerHTML = `<b>Precio Venta menos el ${inputPorcentaje2}% = ${brutoAl21MenosDescuento}</b>`;
-  }
+  
+  let cantidad = parseInt(document.getElementById("cantidad2").value);
+
+  let masCantidad = brutoAl21MenosDescuento * cantidad;
+
+  document.getElementById("c2").innerHTML = `El precio * ${cantidad} es igual a ${masCantidad}`
+}
   
   function limpiar() {
     location.reload();
